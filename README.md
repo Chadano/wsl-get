b4a71a0cd1eea5c4b50942067dcc01ef7d5e8ca4
Chadano <airriflepellet@gmail.com>
2022-07-14 03:48:39 +0100

    fix: bugs

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # wsl-get
-A collection of .debs I require for WSL2
+
+wsl-get is a tool which handles a collection of .deb packages that I (and contributors) use often when utilising WSL2. Rather than manually typing a wget command to download package files, use this little tool instead!
+
+## Installation
+
+This assumes that you are already running a debian-based WSL2 instance.
+
+```sh
+git clone https://github.com/chadano/wsl-get
+cd wsl-get/
+python3 wsl-get.py
+```
+
+## Credits
+
+Created by Chadano
--- wsl-get.py
+++ wsl-get.py
@@ -2,13 +2,16 @@ import os
 from os import *
 
 while True:
-    arg = input("Please type your input: \n help - List help commands")
+    arg = input("Please type your input (type help to list commands):\n")
     if arg == "help":
         print("inst:<package> - Installs a package")
         print("list:pkgs - List packages")
+        print("exit")
     if arg == "list:pkgs":
         print("wsl-get Packages:")
         print("gh - GitHub CLI")
     if arg == "inst:gh":
         os.system('wget https://github.com/cli/cli/releases/download/v2.14.1/gh_2.14.1_linux_amd64.deb')
-        os.system('sudo apt install gh_2.14.1_linux_amd64.deb')
+        os.system('sudo apt install ./gh_2.14.1_linux_amd64.deb')
+    if arg == "exit":
+        quit()
